940d2799a0a3664d83cc82054e69144be5c510d9
Jonathan Mazin <jmazin@brainstation.io>
2023-07-19 12:53:16 -0400

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5050;
 
+app.use(cors());
 app.use(express.json());
 
 const warehouseRoutes = require('./routes/warehouseRoute');
